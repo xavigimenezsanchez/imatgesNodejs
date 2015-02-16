58c1b9099e6c9048fb8c5ee5a0febc97685b0288
xavigimenezsanchez <javiergimenezsanchez@gmail.com>
2015-02-16 10:34:24 +0000

    Primer commit

---
+++ controllers/static.js
@@ -0,0 +1,12 @@
+var express= require("express");
+var router = express.Router();
+var options = {
+    root: __dirname + "../../layouts"
+};
+
+router.use(express.static(__dirname+"/../assets"));
+router.get("/", function(req, res, next) {
+    res.sendFile("imatges.html", options);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require("multer");  //necessari per manegar multipart/form-data
+var app = express();
+
+app.use("/api/json",bodyParser.json());  // La api/json funcionarà amb bodyParser
+app.use("/api/json",require("./controllers/api/json"));
+app.use("/api/imatges",multer( {dest : "./uploads/"})); // uploads és la carpeta on aniran els fitxer quen es fa post
+app.use("/api/imatges", require("./controllers/api/imatges")); // Per carregar imatges farem servir mutter
+
+app.use("/",require("./controllers/static"));
+
+
+app.listen(process.env.PORT, function() {
+    console.log('Server listening on', process.env.PORT);
+});
+
+//http://blog.ragingflame.co.za/2015/2/6/resizing-images-in-nodejs-applications
